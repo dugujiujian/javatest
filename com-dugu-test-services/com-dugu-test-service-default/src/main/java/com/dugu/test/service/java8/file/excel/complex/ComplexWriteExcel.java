224d49cd4c1ffc92a8a49b37864b5e138bc54af7
刺魂 <zhaohaihua@yundongcloud.com>
2023-02-20 22:30:24 +0800

    feat:模版模式使用

--- com-dugu-test-services/com-dugu-test-service-default/src/main/java/com/dugu/test/service/java8/file/excel/complex/ComplexWriteExcel.java
+++ com-dugu-test-services/com-dugu-test-service-default/src/main/java/com/dugu/test/service/java8/file/excel/complex/ComplexWriteExcel.java
@@ -1,5 +1,9 @@
-package com.dugu.test.service.java8.file.excel.complex;/**
-* @author cihun
-* @date 2023-02-19 1:06 下午
-*/public class ComplexWriteExcel {
+package com.dugu.test.service.java8.file.excel.complex;
+
+/**
+ * @author cihun
+ * @date 2023-02-19 1:06 下午
+ */
+public class ComplexWriteExcel {
+
 }
--- com-dugu-test-services/com-dugu-test-service-default/src/test/java/com/dugu/test/service/java8/file/excel/complex/ComplexWriteExcelTest.java
+++ com-dugu-test-services/com-dugu-test-service-default/src/test/java/com/dugu/test/service/java8/file/excel/complex/ComplexWriteExcelTest.java
@@ -1,7 +1,36 @@
+package com.dugu.test.service.java8.file.excel.complex;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.dugu.test.service.java8.file.excel.ExcelTestData;
 import junit.framework.TestCase;
+import org.junit.Test;
+
 /**
-* @author cihun
-* @date 2023-02-19 1:07 下午
-*/public class ComplexWriteExcelTest extends TestCase {
-  
+ * @author cihun
+ * @date 2023-02-19 1:07 下午
+ */
+public class ComplexWriteExcelTest extends TestCase {
+
+
+
+    @Test
+    public void writeExcel() {
+
+//        String fileName = "//Users/zhaohaihua/Documents/complex.xlsx";
+//        ExcelWriter excelWriter = EasyExcel.write(fileName, ExcelTestData.class).build();
+//        WriteSheet writeSheet = EasyExcel.writerSheet("test").build();
+//        excelWriter.write(dataFromFile(), writeSheet).h;
+//        excelWriter.finish();
+
+    }
+
+    @Test
+    public void test(){
+//        EasyExcel.write(response.getOutputStream())
+//                .head(header)
+//                .sheet("模板").doWrite(Collections.EMPTY_LIST);
+    }
+
 }

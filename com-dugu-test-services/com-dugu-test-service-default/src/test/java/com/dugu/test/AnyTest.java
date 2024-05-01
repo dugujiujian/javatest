02e2f84562de70164f2dfa9515a323188125ffeb
刺魂 <zhaohaihua@163.com>
2024-05-01 20:20:05 +0800

    feat:生成文档

--- com-dugu-test-services/com-dugu-test-service-default/src/test/java/com/dugu/test/AnyTest.java
+++ com-dugu-test-services/com-dugu-test-service-default/src/test/java/com/dugu/test/AnyTest.java
@@ -1,10 +1,10 @@
 package com.dugu.test;
 
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @author cihun
@@ -12,19 +12,31 @@ import java.util.Map;
  */
 public class AnyTest {
 
+    public static void main(String[] args) {
+//        String input = "a/b{/}b/c{/}{/}c";
+//        String output = convertString(input);
+//        System.out.println(output);
 
-
-    @Test
-    public void test(){
-
-        Map<String,Object> map=new HashMap<>();
-
-        AnyTest d=(AnyTest)map.get("A");
-
-        System.out.println(StringUtils.joinWith("_", "test", "dd"));
+        BigDecimal c = (new BigDecimal("0.34"))
+                .multiply(new BigDecimal("10")).setScale(0, RoundingMode.CEILING);
+        System.out.println(c.toString());
 
     }
 
+    public static String convertString(String input) {
+        String[] parts = StringUtils.replace(input, "{/}", "#@#").split("/");
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            if (part.contains("#@#")) {
+                part = part.replace("#@#", "/");
+            }
+            output.append(part).append(" ");
+        }
+
+        return output.toString();
+    }
 
 }
 
